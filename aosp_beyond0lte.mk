d5d97dcd7500626f4ac6c933d0c955adaf508e3f
Luke Williams <luke@expressluke.com>
2022-12-24 14:56:05 +0000

    beyond0lte: It's Pixel Experience time!

---
+++ aosp_beyond0lte.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2021 The aospOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+## Product API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+## Inherit from beyond0lte device
+$(call inherit-product, device/samsung/beyond0lte/device.mk)
+
+## Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
+
+## Inherit some common aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Device identifier, this must come after all inclusions
+PRODUCT_NAME := aosp_beyond0lte
+PRODUCT_DEVICE := beyond0lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G970F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
